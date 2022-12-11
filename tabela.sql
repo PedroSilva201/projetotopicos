638aee00d491020db4c72fea65a2b6fddb5fbef8
Pedro Reginaldo Tome Silva <67759198+PedroSilva201@users.noreply.github.com>
2022-12-11 17:13:23 -0300

    Update tabela.sql

--- tabela.sql
+++ tabela.sql
@@ -6,3 +6,7 @@ create table clt_cliente (
     clt_email varchar(100) not null unique,
     clt_telefone int not null
 );
+
+insert into clt_cliente (clt_nome, clt_email, clt_telefone)
+    values ('Carmen', 'carmen@sandiego.com', 997114343),
+           ('Banjo', 'banjo@kazooie.com', 997224444);
